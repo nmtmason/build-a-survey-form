c07b218180217b1cf6dd566b997b13eacc53dde3
Nicholas Mason <nmt.mason@googlemail.com>
2018-06-04 11:00:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# [Build a Survey Form](https://learn.freecodecamp.org/responsive-web-design/responsive-web-design-projects/build-a-survey-form)
+
+Part of the [freecodecamp](https://www.freecodecamp.com) curriculum.
+
+## Objective
+
+Build a CodePen.io app that is functionally similar to this: https://codepen.io/FreeCodeCamp/full/NNvBQW/.
+
+## Solution
+
+* Built using HTML, CSS, JS, [BEM](http://getbem.com/) and [Normalize.css](https://necolas.github.io/normalize.css/).
+* Inspired by [this Dribble](https://dribbble.com/shots/3963594-Form-builder-application).
+* Custom radio groups and checkboxes are used throughout the application.
+* This enabled a deep dive into ARIA accessibility for these elements.
+  * [ARIA Radio Group Example](https://www.w3.org/TR/wai-aria-practices-1.1/examples/radio/radio-1/radio-1.html)
+  * [ARIA Checkbox Example](https://www.w3.org/TR/wai-aria-practices-1.1/examples/checkbox/checkbox-1/checkbox-1.html)
+* Functionality for these items is provided by JavaScript. ES6 classes are used to provide behaviour for all dynamic elements on the page.
+
+## Development tools
+
+* [BrowserSync](https://github.com/Browsersync/browser-sync) is used to provide live reloading during development.
---
+++ js/index.js
@@ -0,0 +1,215 @@
+class QuestionElement {
+  constructor(questionGroup, el, index) {
+    this.questionGroup = questionGroup;
+    this.el = el;
+    this.index = index;
+    this.el.addEventListener('click', this.handleClick.bind(this));
+    this.el.addEventListener('focusin', this.handleFocusIn.bind(this));
+  }
+
+  handleClick(event) {
+    this.questionGroup.setSelected(this.index);
+  }
+
+  handleFocusIn(event) {
+    this.questionGroup.setSelected(this.index);
+  }
+}
+
+class QuestionGroup {
+  constructor(questionElements, selectedClassName) {
+    this.selectedClassName = selectedClassName;
+    this.questionElements = questionElements.map(
+      (questionElement, index) =>
+        new QuestionElement(this, questionElement, index)
+    );
+  }
+
+  setSelected(index) {
+    for (let i = 0; i < this.questionElements.length; i++) {
+      let el = this.questionElements[i].el;
+      if (index === i) {
+        el.classList.add(this.selectedClassName);
+      } else {
+        el.classList.remove(this.selectedClassName);
+      }
+    }
+  }
+}
+
+class AriaRadioElement {
+  constructor(radioGroup, label, input, index) {
+    this.radioGroup = radioGroup;
+    this.label = label;
+    this.input = input;
+    this.index = index;
+    this.label.addEventListener('keydown', this.handleKeyDown.bind(this));
+    this.label.addEventListener('click', this.handleClick.bind(this));
+  }
+
+  handleKeyDown(event) {
+    switch (event.keyCode) {
+      case 32: // SPACE
+        event.preventDefault();
+        this.radioGroup.select(this.index);
+        break;
+      case 37: // LEFT
+      case 38: // UP
+        event.preventDefault();
+        this.radioGroup.selectPrevious(this.index);
+        break;
+      case 39: // RIGHT
+      case 40: //DOWN
+        event.preventDefault();
+        this.radioGroup.selectNext(this.index);
+        break;
+    }
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    this.radioGroup.select(this.index);
+  }
+}
+
+class AriaRadioGroup {
+  constructor(radioElements) {
+    this.radioElements = radioElements.map(
+      (radioElement, index) =>
+        new AriaRadioElement(
+          this,
+          radioElement,
+          radioElement.previousElementSibling,
+          index
+        )
+    );
+    this.selected = 0;
+  }
+
+  selectNext(index) {
+    this.selected = index + 1;
+    if (this.selected === this.radioElements.length) {
+      this.selected = 0;
+    }
+    this.select();
+  }
+
+  selectPrevious(index) {
+    this.selected = index - 1;
+    if (this.selected < 0) {
+      this.selected = this.radioElements.length - 1;
+    }
+    this.select();
+  }
+
+  select(index = this.selected) {
+    for (let radioElement of this.radioElements) {
+      radioElement.label.setAttribute('aria-checked', false);
+      radioElement.label.setAttribute('tabindex', -1);
+      radioElement.input.removeAttribute('checked');
+    }
+    let radio = this.radioElements[index];
+    radio.label.setAttribute('aria-checked', true);
+    radio.label.setAttribute('tabindex', 0);
+    radio.input.setAttribute('checked', true);
+    radio.label.focus();
+  }
+}
+
+class AriaCheckboxElement {
+  constructor(label, input) {
+    this.label = label;
+    this.input = input;
+    this.label.addEventListener('keydown', this.handleKeyDown.bind(this));
+    this.label.addEventListener('click', this.handleClick.bind(this));
+  }
+
+  toggle() {
+    if (this.input.hasAttribute('checked')) {
+      this.label.removeAttribute('aria-checked');
+      this.input.removeAttribute('checked');
+    } else {
+      this.label.setAttribute('aria-checked', true);
+      this.input.setAttribute('checked', true);
+    }
+  }
+
+  handleKeyDown(event) {
+    switch (event.keyCode) {
+      case 32: // SPACE
+        event.preventDefault();
+        this.toggle();
+    }
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    this.toggle();
+  }
+}
+
+class Form {
+  constructor(form, fields, requiredFields) {
+    this.form = form;
+    this.requiredFields = requiredFields;
+    this.fields = fields;
+    this.form.addEventListener('submit', this.handleSubmit.bind(this));
+  }
+
+  validateText(elements) {
+    return elements.every(element => Boolean(element.value));
+  }
+
+  validateRadio(elements) {
+    return elements.some(element => Boolean(element.hasAttribute('checked')));
+  }
+
+  valid() {
+    return this.requiredFields.every(requiredField => {
+      let fields = this.fields.filter(
+        field => field.name === requiredField.name
+      );
+      return requiredField.type === 'text'
+        ? this.validateText(fields)
+        : requiredField.type === 'radio'
+          ? this.validateRadio(fields)
+          : false;
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    alert(
+      this.valid()
+        ? 'Your form has been submitted!'
+        : 'There are errors with your form'
+    );
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  let questions = document.querySelectorAll('.question');
+  new QuestionGroup(Array.from(questions), 'question--selected');
+
+  let radioGroups = document.querySelectorAll('[role="radiogroup"]');
+  radioGroups.forEach(
+    radioGroup =>
+      new AriaRadioGroup(
+        Array.from(radioGroup.querySelectorAll('[role="radio"]'))
+      )
+  );
+
+  let checkboxes = document.querySelectorAll('[role="checkbox"]');
+  checkboxes.forEach(
+    checkbox =>
+      new AriaCheckboxElement(checkbox, checkbox.previousElementSibling)
+  );
+
+  let form = document.querySelector('form');
+  new Form(form, Array.from(form.querySelectorAll('input')), [
+    { name: 'name', type: 'text' },
+    { name: 'email', type: 'text' },
+    { name: 'age', type: 'text' },
+    { name: 'recommend', type: 'radio' }
+  ]);
+});
